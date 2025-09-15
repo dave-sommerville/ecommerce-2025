3d9977bae7ba63ee4da2ae9181148630152561bf
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-09-15 10:22:54 -0500

    Preview card updated in index.html

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -40,7 +40,7 @@ export default function HeroBanner({ setHeroHeight }) {
       <section className="title-section">
         <div className="flex">
           <img className="hero-logo" src={logo}></img>
-          <h1 class="flex center">Altas Goods</h1>
+          <h1 class="flex center"><span>Altas</span> Goods</h1>
         </div>
       <h2>Anything. Anywhere.</h2>
       </section>
